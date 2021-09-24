3a6fa75c42a1c3ad29f5fa4fe4193f1dee112f79
Rohith ch <myselfrohith@outlook.com>
2021-09-24 11:53:02 +0530

    fix error in changing psd

--- email/index.js
+++ email/index.js
@@ -20,7 +20,7 @@ const forgotPasswordTemplate = (userToken, email) => ({
   to: email,
   from: "company.social.network.org@gmail.com",
   subject: "Account Password Reset",
-  html: ` You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n
+  html: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n
           Please click on the following link, or paste this into your browser to complete the process\n'If you did not request this, please ignore this email and your password will remain unchanged.\n'
       <a href=http://localhost:3000/reset_password/${userToken}>click here</a>`,
 });
